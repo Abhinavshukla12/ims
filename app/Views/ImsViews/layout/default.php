1774c97b3e1671175a87017898643e7a2caff0f1
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-07-05 00:17:42 +0530

    navbar update

--- app/Views/ImsViews/layout/default.php
+++ app/Views/ImsViews/layout/default.php
@@ -12,12 +12,10 @@
     <link href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
     <!-- Bootswatch CSS -->
     <link href="<?= base_url('node_modules/bootswatch/dist/zephyr/bootstrap.min.css') ?>" rel="stylesheet">
-    <!--custom css-->
 
     <!-- Load custom CSS -->
-    
-    <style>
-    /* General Navbar Styling */
+<style>
+/* General Navbar Styling */
 .navbar {
     background-color: #333;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
@@ -50,109 +48,109 @@
 }
 
 .navbar-nav .nav-link:hover {
+    background-color: #5f948e;
     color: black;
-    background-color: #4a7375;
     border-radius: 4px;
 }
-</style>
-    <!-- Bootstrap css -->
-    <?php
-    if (isset($css)) {
-        foreach ($css as $style):
-            echo '<link href="' . base_url('' . $style) . '" rel="stylesheet">';
-        endforeach;
-    }
-    ?>
-</head>
 
+.dropdown-menu{
+    width: 200px;
+    margin-top: 20px;
+    margin-left: 10px;
+    background-color: #667be3;
+}
+</style>
+
+<!-- Bootstrap css -->
+<?php
+if (isset($css)) {
+    foreach ($css as $style):
+        echo '<link href="' . base_url('' . $style) . '" rel="stylesheet">';
+    endforeach;
+}
+?>
+
+</head>
 <body>
+
 <!-- Primary Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <a class="navbar-brand" href="<?=base_url('ims/home')?>">Factory Management System</a> <!-- Navbar Brand -->
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?=base_url('ims/home')?>">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?=base_url('ims/about')?>">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?=base_url('ims/contact')?>">Contact</a>
-                </li>
-                <li class="nav-item">
+    <a class="navbar-brand" href="<?=base_url('ims/home')?>">Factory Management System</a> <!-- Navbar Brand -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?=base_url('ims/home')?>">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?=base_url('ims/about')?>">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?=base_url('ims/contact')?>">Contact</a>
+            </li>
+            <!-- Services Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Services
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="nav-link" href="<?=base_url('ims/stock')?>">Stocks Management</a>
+                    <a class="nav-link" href="<?=base_url('ims/sales')?>">Sales Management</a>
+                    <a class="nav-link" href="<?=base_url('ims/purchase_order')?>">Purchase Management</a>
+                    <a class="nav-link" href="<?=base_url('ims/item_management')?>">Item Management</a>
+                    <a class="nav-link" href="<?=base_url('ims/suppliers')?>">Supplier Management</a>
+                    <a class="nav-link" href="<?=base_url('ims/document')?>">Document Management</a>
+                    <a class="nav-link" href="<?=base_url('ims/warehouse')?>">Warehouse Management</a>
+                </div>
+            </li>
+            <!-- Your Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Your Profile
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="nav-link" href="<?=base_url('ims/profile')?>">Profile</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-<!-- Secondary Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-info">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/stock')?>">Stocks Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/sales')?>">Sales Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/purchase_order')?>">Purchase Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/item_management')?>">Item Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/suppliers')?>">Supplier Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/document')?>">Document Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/warehouse')?>">Warehouse Management</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?=base_url('ims/logout')?>">Logout</a>
+                    <a class="nav-link" href="<?=base_url('ims/logout')?>">Logout</a>
+                </div>
             </li>
         </ul>
     </div>
 </nav>
 
+<!-- the main content goes here -->
+<div class="container">
+    <?= $this->renderSection('content') ?>
+</div>
 
-
-
-    <!-- the main content goes here -->
-    <div class="container">
-        <?= $this->renderSection('content') ?>
-    </div>
-
-    <script src="<?= base_url('node_modules/jquery/dist/jquery.min.js') ?>"></script>
-  <!-- Bootstrap JS and dependencies -->
-  <script src="<?= base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('node_modules/jquery/dist/jquery.min.js') ?>"></script>
+<!-- Bootstrap JS and dependencies -->
+<script src="<?= base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/printThis/1.15.0/printThis.min.js"></script>
 
+<!-- jQuery, Popper.js, and Bootstrap JS -->
+<script type="text/javascript">
+    $.ajaxPrefilter(function (options, originalData, xhr) {
+        var data = "<?php echo  csrf_token(). '=' . csrf_hash(); ?>" + "&random=" + Math.random();
+        if (options.data) {
+            options.data += "&" + data;
+        } else {
+            options.data = data;
+        }
+    });
+</script>
 
-    <!-- jQuery, Popper.js, and Bootstrap JS -->
-    <script type="text/javascript">
-        $.ajaxPrefilter(function (options, originalData, xhr) {
-            var data = "<?php echo  csrf_token(). '=' . csrf_hash(); ?>" + "&random=" + Math.random();
-            if (options.data) {
-                options.data += "&" + data;
-            } else {
-                options.data = data;
-            }
-        });
-    </script>
+<!-- Load custom JS -->
+<?php
+if (isset($js)) {
+    foreach ($js as $script):
+        echo '<script src="' . base_url('' . $script) . '"></script>';
+    endforeach;
+}
+?>
 
-    <!-- Load custom JS -->
-    <?php
-    if (isset($js)) {
-        foreach ($js as $script):
-            echo '<script src="' . base_url('' . $script) . '"></script>';
-        endforeach;
-    }
-    ?>
 </body>
-
 </html>
