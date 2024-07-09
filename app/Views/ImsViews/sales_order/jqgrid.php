ce4104832400eed17bd07602a779a19160385c7f
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-07-09 22:46:56 +0530

    jqgrid links updated

--- app/Views/ImsViews/sales_order/jqgrid.php
+++ app/Views/ImsViews/sales_order/jqgrid.php
@@ -10,7 +10,11 @@
     <div id="pager"></div>
 </div>
 </body>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.9/xlsx.full.min.js"></script>
+<!-- Include jsPDF -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<!-- Include jsPDF autoTable plugin -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.19/jspdf.plugin.autotable.min.js"></script>
 <style>
 body{
     background-color: #046169;
